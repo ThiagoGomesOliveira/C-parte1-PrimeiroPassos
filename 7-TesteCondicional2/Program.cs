028b80e20cc49cea5e1368410a16543d50c053ee
ThiagoGomesOliveira <thiago.blu1988@gmail.com>
2018-08-26 18:42:42 -0300

    alura

---
+++ 7-TesteCondicional2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7_TesteCondicional2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Testando Condicional");
+
+            int idade = 15;
+            int qtdPessoas = 0;
+
+            if (idade >= 18 || qtdPessoas >= 2)
+            {
+                Console.WriteLine("Maior de idade !");
+            }
+            else
+            {
+                Console.WriteLine("Menor de idade");
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ 9-AliquotaIfs/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9_AliquotaIfs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double salario  =3300.0;
+            double salarioDesconto;
+
+            if (salario >=  1900.0  && salario <= 2800.0)
+            {
+                salarioDesconto = salario - 142;
+                Console.WriteLine(salarioDesconto);
+                Console.ReadLine();
+            }
+            if (salario >= 2800.01 && salario <= 3751.0 )
+            {
+                salarioDesconto = salario - 350;
+                Console.WriteLine(salarioDesconto);
+                Console.ReadLine();
+            }
+            if (salario >= 3751.01 && salario <= 4664.00)
+            {
+                salarioDesconto = salario - 636;
+                Console.WriteLine(salarioDesconto);
+                Console.ReadLine();
+
+            }
+
+
+
+        }
+    }
+}
